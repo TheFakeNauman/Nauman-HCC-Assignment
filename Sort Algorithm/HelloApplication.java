151f030d3a693c68dac755fe756f2c83be37c7a4
TheFakeNauman <124740397+TheFakeNauman@users.noreply.github.com>
2023-03-29 11:46:49 -0400

    Add files via upload

---
+++ Sort Algorithm/HelloApplication.java
@@ -0,0 +1,93 @@
+//Nauman Choudhary
+
+
+package com.example.sortingalgorithm;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray(10);
+        System.out.println("Before sorting: " + Arrays.toString(arr));
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println("After Merge Sort: " + Arrays.toString(arr));
+
+        int[] arr1 = generateRandomArray(10);
+        System.out.println("Before sorting: " + Arrays.toString(arr1));
+        insertionSort(arr1);
+        System.out.println("After Insertion Sort: " + Arrays.toString(arr1));
+    }
+
+
+    public static int[] generateRandomArray(int size) {
+        int[] arr = new int[size];
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        return arr;
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+}
